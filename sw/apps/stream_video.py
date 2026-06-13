e304924c200ecdfc0c48daad699437c41684199d
koliazak <vrmsumy3@gmail.com>
2026-06-13 17:28:04 +0200

    update: separate detection and streaming logic

--- sw/apps/stream_video.py
+++ sw/apps/stream_video.py
@@ -10,6 +10,7 @@ import time
 import threading
 import logging
 import signal
+import select
 
 
 """MJPEG stream from /dev/shm/frame.raw"""
@@ -31,10 +32,14 @@ W, H = 640, 480
 FRAME_SIZE = W * H * 2
 STALE_THRESHOLD_S = 2.0   # warn if no new frame for 2 seconds
 
+
 latest_jpeg = None
 latest_bgr  = None
+latest_annotated_jpeg = None
 jpeg_lock = threading.Lock()
+detect_lock = threading.Lock()
 frame_counter = 0
+detect_counter = 0
 
 
 def rgb565_to_bgr(raw):
@@ -52,43 +57,99 @@ def encoder(logger: Logger):
     if not os.path.exists(FIFO_PATH):
         os.mkfifo(FIFO_PATH)
 
-    logger.info("[encoder] waiting for frames via FIFO")
+    logger.info("waiting for frames via FIFO")
 
-    fd = os.open(FIFO_PATH, os.O_RDONLY)
+    fd = os.open(FIFO_PATH, os.O_RDONLY | os.O_NONBLOCK)
 
     try:
         while True:
+            ready, _, _ = select.select([fd], [], [], 0.1)
+            if ready:
+                data = os.read(fd, 1)
+                if not data:
+                    break
+                try:
+                    with open(FRAME_PATH, "rb") as f:
+                        raw = f.read()
 
-            data = os.read(fd, 1)
+                    if len(raw) != FRAME_SIZE:
+                        logger.warning(f"bad frame size {len(raw)}, expected {FRAME_SIZE}")
+                        continue
 
-            if not data:
-                break
+                    bgr = rgb565_to_bgr(raw)
+                    ret, jpeg = cv2.imencode(".jpg", bgr, [int(cv2.IMWRITE_JPEG_QUALITY), 65])
 
-            try:
-                with open(FRAME_PATH, "rb") as f:
-                    raw = f.read()
+                    if ret:
+                        with jpeg_lock:
+                            global latest_jpeg, latest_bgr, frame_counter
+                            latest_jpeg = jpeg.tobytes()
+                            latest_bgr = bgr.copy()
+                            frame_counter += 1
 
-                if len(raw) != FRAME_SIZE:
-                    logger.warning(f"bad frame size {len(raw)}, expected {FRAME_SIZE}")
-                    continue
+                except Exception as e:
+                    logger.error(e)
 
-                bgr = rgb565_to_bgr(raw)
-                ret, jpeg = cv2.imencode(".jpg", bgr, [int(cv2.IMWRITE_JPEG_QUALITY), 65])
-
-                if ret:
-                    with jpeg_lock:
-                        global latest_jpeg, latest_bgr, frame_counter
-                        latest_jpeg = jpeg.tobytes()
-                        latest_bgr = bgr.copy()
-                        frame_counter += 1
-
-            except Exception as e:
-                logger.error(e)
     except (KeyboardInterrupt, SystemExit):
         logger.info("Encoder stopped")
     finally:
         os.close(fd)
 
+def detector(logger: Logger):
+    while True:
+        try:
+            timeout_retry = time.time()
+            last_detect = -1
+            session = requests.Session()
+
+            while True:
+                with jpeg_lock:
+                    if latest_bgr is None or latest_jpeg is None:
+                        time.sleep(0.05)
+                        continue
+                    if last_detect == frame_counter:
+                        time.sleep(0.01)
+                        continue
+                    bgr = latest_bgr.copy()
+                    jpeg = latest_jpeg
+                    last_detect = frame_counter
+
+                coords = None
+                if time.time() - timeout_retry > 5:
+                    try:
+                        with session as s:
+                                res = s.post(DETECT_ADDR, files={"file": jpeg}, timeout=3)
+                                res = res.json()
+                                coords = res.get("coordinates")
+                    except Exception as e:
+                        logger.error(f"Couldn't reach detection endpoint, retrying in 5 seconds \n{e}")
+                        timeout_retry = time.time()
+
+                if coords:
+                    coords = [
+                        [tuple(box[:2]), tuple(box[2:])] for box in coords
+                    ]
+                    for i in coords:
+                        cv2.rectangle(bgr, i[0], i[1], (255, 0, 0), 2)
+
+                ret, jpeg_tmp = cv2.imencode(".jpg", bgr, [int(cv2.IMWRITE_JPEG_QUALITY), 65])
+
+                with detect_lock:
+                    global latest_annotated_jpeg, detect_counter
+                    if not latest_annotated_jpeg:
+                        latest_annotated_jpeg = jpeg
+
+                    if ret:
+                        detect_counter += 1
+                        latest_annotated_jpeg = jpeg_tmp.tobytes()
+                    else:
+                        logger.error("Couldn't compress jpeg")
+
+        except (KeyboardInterrupt, SystemExit):
+            logger.info("Detector stopped")
+
+        except Exception as ex:
+            logger.error(ex)
+            time.sleep(0.05)
 
 HTML = b'''<!DOCTYPE html>
 <html><body style="background:#111;text-align:center;">
@@ -115,32 +176,14 @@ class Handler(http.server.BaseHTTPRequestHandler):
             last_seen = -1
 
             while True:
-                session = requests.Session()
                 try:
-                    with jpeg_lock:
-                        if frame_counter == last_seen:
+                    with detect_lock:
+                        if detect_counter == last_seen:
                             time.sleep(0.01)
                             continue
-                        jpeg = latest_jpeg
-                        last_seen = frame_counter
+                        jpeg = latest_annotated_jpeg
+                        last_seen = detect_counter
 
-                    with session as s:
-                        res = s.post(DETECT_ADDR, files={"file": jpeg}, timeout=3)
-                        res = res.json()
-                    coords = res.get("coordinates")
-                    if coords:
-                        coords = [
-                            [tuple(box[:2]), tuple(box[2:])] for box in coords
-                        ]
-                        for i in coords:
-                            cv2.rectangle(latest_bgr, i[0], i[1], (255,0,0), 2)
-
-                    ret, jpeg_tmp = cv2.imencode(".jpg", latest_bgr, [int(cv2.IMWRITE_JPEG_QUALITY), 65])
-
-                    if ret:
-                        jpeg = jpeg_tmp
-                    else:
-                        logger.error("Couldn't compress jpeg")
                     self.wfile.write(b'--frame\r\n')
                     self.wfile.write(b'Content-Type: image/jpeg\r\n')
                     self.wfile.write(f'Content-Length: {len(jpeg)}\r\n'.encode())
@@ -167,6 +210,9 @@ if __name__ == '__main__':
         time.sleep(0.1)
 
     threading.Thread(target=encoder, args=[logger.getChild("encoder")], daemon=True).start()
+    time.sleep(0.1)
+    threading.Thread(target=detector, args=[logger.getChild("detector")], daemon=True).start()
+
     time.sleep(0.2)
 
 
@@ -174,6 +220,7 @@ if __name__ == '__main__':
         try:
             logger.info("Server started")
             srv.daemon_threads = True
+            srv.allow_reuse_address = True
             srv.serve_forever()
         except (KeyboardInterrupt, SystemExit):
             logger.error("Shutdown signal received")
